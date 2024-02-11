b52ad5a386ccba03bc4bb8ab07d923a6cbcc23f8
Alex Cabrera <alex.cabrera@gmail.com>
2024-02-10 19:09:50 -0800

    feat: add user to db on login with social

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,10 +1,22 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export const GET = async ({ url, locals: { supabase } }) => {
 	const code = url.searchParams.get('code');
 
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
+	}
+
+	const userResponse = await supabase.auth.getUser();
+	if (userResponse.error) {
+		error(500, userResponse.error.message);
+	}
+
+	const user = userResponse.data.user;
+	const { error: err } = await supabase.from('users').insert({ id: user?.id, email: user?.email });
+
+	if (err) {
+		error(500, err.message);
 	}
 
 	redirect(303, '/home');
