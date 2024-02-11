4c07191d73a8033b55f7310edc080b39a1a5f0d4
Alex Cabrera <alex.cabrera@gmail.com>
2024-02-10 19:29:31 -0800

    better setup

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,19 +1,20 @@
 import { error, redirect } from '@sveltejs/kit';
 
-export const GET = async ({ url, locals: { supabase } }) => {
+export const GET = async ({ url, locals: { supabase, getSession } }) => {
 	const code = url.searchParams.get('code');
 
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
 
-	const userResponse = await supabase.auth.getUser();
-	if (userResponse.error) {
-		error(500, userResponse.error.message);
+	const session = await getSession();
+	if (!session) {
+		error(500, 'Error creating user');
 	}
 
-	const user = userResponse.data.user;
-	const { error: err } = await supabase.from('users').insert({ id: user?.id, email: user?.email });
+	const { error: err } = await supabase
+		.from('users')
+		.insert({ id: session.user?.id, email: session.user?.email });
 
 	if (err) {
 		error(500, err.message);
