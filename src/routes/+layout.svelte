932301fe8ee02052ba79cf807245e744ac243975
Alex Cabrera <alex.cabrera@gmail.com>
2024-02-11 09:12:59 -0800

    feat: add user to db on login with social (#56)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onNavigate } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Tooltip from '$lib/components/ui/Tooltip.svelte';
 	import { tooltipState } from '$lib/tooltip.svelte';
 	import '../app.css';
@@ -16,8 +17,23 @@
 </script>
 
 <svelte:head>
-	<title>endoftext</title>
-	<meta name="description" content="AI-assisted Prompt Editor" />
+	<title>endoftext | AI-powered prompt editor</title>
+	<meta
+		name="description"
+		content="Take the guesswork out of prompt engineering with prompt suggestions, smart re-writing, and synthetic data generation. endoftext is an AI-powered prompt writing assistant that helps you quickly improve your prompts."
+	/>
+	<meta property="og:title" content="endoftext | AI-powered prompt editor" />
+	<meta property="og:site_name" content="app.endoftext.app" />
+	<meta
+		property="og:description"
+		content="Take the guesswork out of prompt engineering with prompt suggestions, smart re-writing, and synthetic data generation. endoftext is an AI-powered prompt writing assistant that helps you quickly improve your prompts."
+	/>
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="/preview.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
 </svelte:head>
 
 <main class="flex h-screen w-screen">
--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,10 +1,23 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
-export const GET = async ({ url, locals: { supabase } }) => {
+export const GET = async ({ url, locals: { supabase, getSession } }) => {
 	const code = url.searchParams.get('code');
 
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
+	}
+
+	const session = await getSession();
+	if (!session) {
+		error(500, 'Error creating user');
+	}
+
+	const { error: err } = await supabase
+		.from('users')
+		.insert({ id: session.user?.id, email: session.user?.email });
+
+	if (err) {
+		error(500, err.message);
 	}
 
 	redirect(303, '/home');
