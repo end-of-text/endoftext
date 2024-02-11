88c91a1ca0db4916e7b041ed8a7af213acf56093
Alex Bäuerle <alex@a13x.io>
2024-02-11 17:01:04 +0100

    set user id

---
+++ src/routes/(app)/+layout.server.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+
+export async function load({ locals: { getSession } }) {
+	const session = await getSession();
+
+	if (!session) {
+		error(401, { message: 'Forbidden' });
+	}
+
+	return {
+		userId: session.user.id
+	};
+}
