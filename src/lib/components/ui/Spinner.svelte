a3ac271fb2f46e7c83eca652d50f6955f6d1d438
Alex Cabrera <alex.cabrera@gmail.com>
2024-02-17 09:15:14 -0800

    feat: custom spinner (#84)
    Co-authored-by: Alex Bäuerle <alex@a13x.io>

--- src/lib/components/ui/Spinner.svelte
+++ src/lib/components/ui/Spinner.svelte
@@ -1,25 +1,37 @@
-<script lang="ts">
-	let { classNames = '' } = $props<{
-		classNames?: string;
-	}>();
-</script>
-
-<div class={classNames} role="status">
-	<svg
-		aria-hidden="true"
-		class="h-5 w-5 animate-spin fill-blue-600 text-gray-100 dark:text-gray-600"
-		viewBox="0 0 100 101"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M5.29412 4.78259L2 9.88404L5.29412 14.9855"
+		stroke="#FF7878"
+		stroke-width="2.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
 	>
-		<path
-			d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-			fill="currentColor"
+		<animate
+			attributeName="d"
+			values="M5.29412 4.78259L2 9.88404L5.29412 14.9855;M5.29419 4.78259L5.29412 10L5.29419 14.9855;M5.29412 4.78259L2 9.88404L5.29412 14.9855"
+			dur="3s"
+			repeatCount="indefinite"
+			keyTimes="0; 0.5; 1"
+			keySplines="0.42 0 0.58 1; 0.42 0 0.58 1"
+			calcMode="spline"
 		/>
-		<path
-			d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-			fill="currentFill"
+	</path>
+	<path d="M10 2V18" stroke="#FF7878" stroke-width="2.5" stroke-linecap="round" />
+	<path
+		d="M14.7059 14.9855L18 9.88402L14.7059 4.78257"
+		stroke="#FF7878"
+		stroke-width="2.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<animate
+			attributeName="d"
+			values="M14.7059 14.9855L18 9.88402L14.7059 4.78257;M14.7058 14.9855L14.7059 10L14.7058 4.78257;M14.7059 14.9855L18 9.88402L14.7059 4.78257"
+			dur="3s"
+			repeatCount="indefinite"
+			keyTimes="0; 0.5; 1"
+			keySplines="0.42 0 0.58 1; 0.42 0 0.58 1"
+			calcMode="spline"
 		/>
-	</svg>
-	<span class="sr-only">Loading...</span>
-</div>
+	</path>
+</svg>
