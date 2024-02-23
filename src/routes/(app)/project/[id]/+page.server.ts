664c381e5ed5ebab8cba2adb015fae7fe008a2c9
Alex Bäuerle <alex@a13x.io>
2024-02-23 19:15:59 +0100

    feat: add back button to go to a previous prompt version (#102)
    Co-authored-by: Alex Cabrera <alex.cabrera@gmail.com>

--- src/routes/(app)/project/[id]/+page.server.ts
+++ src/routes/(app)/project/[id]/+page.server.ts
@@ -1,58 +1,20 @@
-import { getPredictions } from '$lib/server/predictions/getPredictions';
-import type { Tables } from '$lib/supabase.js';
 import { error, redirect } from '@sveltejs/kit';
 
-export async function load({ depends, locals: { supabase }, params }) {
-	depends('prompt');
+export async function load({ locals: { getSession, supabase }, params }) {
+	const session = getSession();
+	if (!session) {
+		redirect(302, '/login');
+	}
 
-	const projectReq = supabase.from('projects').select('*').eq('id', params.id);
-
-	const promptsReq = supabase
+	const promptReq = await supabase
 		.from('prompts')
 		.select('*')
 		.eq('project_id', params.id)
 		.order('created_at', { ascending: false });
 
-	const instancesReq = await supabase
-		.from('instances')
-		.select('*')
-		.eq('project_id', params.id)
-		.order('id', { ascending: true });
-
-	const [promptsRes, projectRes, instancesRes] = await Promise.all([
-		promptsReq,
-		projectReq,
-		instancesReq
-	]);
-
-	if (promptsRes.error || projectRes.error || instancesRes.error) {
-		error(505, { message: 'Failed to get project, prompts, or instances' });
+	if (promptReq.error || promptReq.data.length === 0) {
+		error(500, 'Failed to load prompt');
+	} else {
+		redirect(302, `/project/${params.id}/${promptReq.data[0].id}`);
 	}
-
-	if (promptsRes.data.length === 0) {
-		redirect(303, '/project/' + params.id + '/new/prompt');
-	}
-
-	const { data: existingPredictions, error: err } = await supabase
-		.from('predictions')
-		.select('*')
-		.eq('prompt_id', promptsRes.data[0].id);
-
-	if (err) {
-		error(500, err.message);
-	}
-
-	const predictions = getPredictions(
-		supabase,
-		existingPredictions,
-		instancesReq.data || [],
-		promptsRes.data[0]
-	);
-
-	return {
-		project: projectRes.data[0] as Tables<'projects'>,
-		prompt: promptsRes.data[0] as Tables<'prompts'>,
-		instances: instancesReq.data as Tables<'instances'>[],
-		predictions
-	};
 }
