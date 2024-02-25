77c318a9c9e62d95f7caa66a884616e4cdd32f0a
Alex Bäuerle <alex@a13x.io>
2024-02-25 13:08:01 +0100

    fix: display correct login error
    fix EOT-84

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -87,8 +87,8 @@
 	{/if}
 	{#if form?.success}
 		<p class="my-2">Check email for confirmation.</p>
-	{:else if form?.error}
-		<p class="my-2">{form?.error}</p>
+	{:else if form && form.success === false}
+		<p class="my-2 text-red-600">{form.message}</p>
 	{/if}
 	<p class="mt-6 text-center text-sm italic text-gray-400">
 		By signing up you agree to our <br /><a href="/tos" class="text-gray-500 hover:text-gray-600"
