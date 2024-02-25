0e75ae948f2e49ffb3cdecc9cfe287a5b6622d79
Alex Bäuerle <alex@a13x.io>
2024-02-25 10:16:06 +0100

    fix: reenable update project name
    fix EOT-85

--- src/routes/(app)/project/[id]/[promptId]/+page.server.ts
+++ src/routes/(app)/project/[id]/[promptId]/+page.server.ts
@@ -70,3 +70,21 @@ export async function load({ depends, locals: { supabase }, params }) {
 		predictions
 	};
 }
+
+export const actions = {
+	updateName: async ({ request, locals: { supabase, getSession }, params }) => {
+		const session = await getSession();
+
+		if (!session) {
+			return {
+				status: 401,
+				body: 'Forbidden'
+			};
+		}
+
+		const formData = await request.formData();
+		const name = formData.get('name') as string;
+
+		await supabase.from('projects').update({ name }).eq('id', params.id);
+	}
+};
