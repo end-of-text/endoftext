08c8f9339b55b18ac5d9384c261cb6d785938256
Alex Cabrera <alex.cabrera@gmail.com>
2024-02-25 00:54:40 -0800

    feat: predict metric (#103)
    Co-authored-by: Alex Bäuerle <alex@a13x.io>

---
+++ src/lib/server/metrics/predictMetric.ts
@@ -0,0 +1,33 @@
+import { OPENAI_API_KEY } from '$env/static/private';
+import type { Tables } from '$lib/supabase';
+import { OpenAILLM } from '../llms/openai';
+
+const METRIC_PROMPT = `
+You pick an evaluation metric for a given AI prompt. 
+
+You can pick from the following metrics:
+"chrf" - measures the bi-gram overlap of two text spans. Good for generative tasks such as summarization, text generation, etc.
+"exact match" - measures if prediction and label are identical. Good for logical or exact tasks such as data parsing
+"fuzzy match" - measures if the prediction has at least part of the label.
+
+Return JSON with the key "output" and the string of the metric.
+`;
+
+export async function predictMetric(prompt: Tables<'prompts'> | undefined): Promise<string> {
+	if (prompt === undefined) {
+		return 'chrf';
+	} else if (prompt.responseFormat === 'json') {
+		return 'exact match';
+	}
+
+	const openai = new OpenAILLM(OPENAI_API_KEY || '');
+	const predictedMetric = await openai.generate(
+		[
+			{ role: 'system', content: METRIC_PROMPT },
+			{ role: 'user', content: prompt.prompt }
+		],
+		{ json: true }
+	);
+	const metricJson = JSON.parse(predictedMetric || "{output: 'chrf'}");
+	return metricJson.output;
+}
