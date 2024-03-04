150536a37fffd17521704db6d0cd1bc27fd549a7
Alex Bäuerle <alex@a13x.io>
2024-03-04 09:27:12 +0100

    fix path

--- src/lib/server/prompts.ts
+++ src/lib/server/prompts.ts
@@ -1,9 +1,12 @@
 import { ENDOFTEXT_API_KEY } from '$env/static/private';
 
 export async function fetchPrompt(projectId: string, promptId: string) {
-	return await fetch(`https://app.endoftext.app/api/serve/project/${projectId}/${promptId}`, {
-		headers: {
-			'x-api-key': ENDOFTEXT_API_KEY
+	return await fetch(
+		`https://app.endoftext.app/api/serve/project/${projectId}/prompt/${promptId}`,
+		{
+			headers: {
+				'x-api-key': ENDOFTEXT_API_KEY
+			}
 		}
-	});
+	);
 }
